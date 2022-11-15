848cf9f1924f1eb32c5b192fde992625ead3f1a4
xu qian su <s3a932003@student.ncut.edu.tw>
2022-11-15 17:58:01 +0800

    練習2 新增資料

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,5 @@
 <?php
+use App\Models\Post;
 use App\Http\Controllers\PostController;
 
 use Illuminate\Support\Facades\Route;
@@ -20,7 +21,12 @@ Route::get('/', function () {
     //return view('post');
     //return view('contact');
     //return view('about');
-    return redirect(route('posts.index'));
+//    return redirect(route('posts.index'));
+    $post = new Post();//先產生Post的物件$post，$post將代表posts資料表的一篇貼文
+    $post->title = 'test title';//指定貼文的title
+    $post->content = 'test content';//指定貼文的content
+    $post->save();//將新貼文$post存入posts資料表
+    return 'Saved, OK!';
 });
 Route::get('posts',[PostController::class, 'index'])->name('posts.index');
 Route::get('post',[PostController::class, 'show'])->name('posts.show');
